eec4223b60f447d9d1b84f4afb8d42a71a1c2fb2
Lazzzare <164561286+LazareChkhartishvili@users.noreply.github.com>
2025-06-27 14:17:49 +0400

    Improve About page mobile responsiveness
    Adjusted font sizes, padding, and layout classes in the About page to enhance readability and layout on smaller screens. These changes ensure better mobile responsiveness while maintaining the original design for larger devices.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,12 +14,12 @@ const About = () => {
         <Category />
         <Works />
         <div className="py-10">
-          <h1 className="text-[#3D334A] text-[64px] leading-[100%] tracking-[-3%]  text-center">
+          <h1 className="text-[#3D334A] text-[32px] md:text-[64px] leading-[100%] tracking-[-3%]  text-center">
             Наша цель
           </h1>
-          <div className="m-10">
-            <div className="bg-[#F9F7FE] py-[34px] pl-10 mb-5 rounded-[15px]">
-              <p className="text-[#3D334A] leading-[120%] text-[24px] font-medium">
+          <div className="md:m-10 m-5">
+            <div className="bg-[#F9F7FE] md:py-[34px] md:pl-10 mb-5 p-4 rounded-[15px]">
+              <p className="text-[#3D334A] leading-[120%] md:text-[24px] text-[14px] font-medium">
                 Мы нацелены на постоянное расширение списка направлений
                 реабилитации и персонализацию программ реабилитации, и будем
                 рады обратной связи с нашими пользователями, чтобы определиться
@@ -27,17 +27,17 @@ const About = () => {
                 дополнительные возможности следует развить.
               </p>
             </div>
-            <div className="flex flex-row items-center gap-5">
-              <div className="bg-[#F9F7FE] p-10 rounded-[15px]">
-                <p className="text-[#3D334A] leading-[120%] text-[24px] font-medium">
+            <div className="flex md:flex-row flex-col items-center gap-5">
+              <div className="bg-[#F9F7FE] md:p-10 p-4 rounded-[15px]">
+                <p className="text-[#3D334A] leading-[120%] md:text-[24px] text-[14px] font-medium">
                   Мы обеспечим вам возможность бесплатных консультаций с
                   квалифицированными кураторами нашей платформы, а также
                   возможность консультаций с ведущими специалистами в клиниках
                   Израиля.
                 </p>
               </div>
-              <div className="bg-[#F9F7FE] p-10 rounded-[15px]">
-                <p className="text-[#3D334A] leading-[120%] text-[24px] font-medium">
+              <div className="bg-[#F9F7FE] md:p-10 p-4 rounded-[15px]">
+                <p className="text-[#3D334A] leading-[120%] md:text-[24px] text-[14px] font-medium">
                   Мы обеспечим вам возможность бесплатных консультаций с
                   квалифицированными кураторами нашей платформы, а также
                   возможность консультаций с ведущими специалистами в клиниках
