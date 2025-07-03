271b2544427aa734f9a83e366ace81a17fa40e10
Lazzzare <164561286+LazareChkhartishvili@users.noreply.github.com>
2025-07-03 14:38:34 +0400

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import Header from "../components/Header";
 import BlogSlider from "../components/BlogSlider";
+import SliderArrows from "../components/SliderArrows";
 
 const Blog = () => {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const blogsPerPage = 3;
+
   return (
     <div>
       <Header />
       <div className="mt-40 flex flex-col gap-20">
-        <BlogSlider />
-        <BlogSlider />
-        <BlogSlider />
-        <BlogSlider />
+      <SliderArrows
+  onScrollLeft={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+  onScrollRight={() => setCurrentPage((prev) => prev + 1)}
+/>
+        <BlogSlider
+          scrollRef={scrollRef}
+          currentPage={currentPage}
+          blogsPerPage={blogsPerPage}
+        />
       </div>
     </div>
   );
