459a05746838459af9bf09bce8226351df5e4b18
Lazzzare <164561286+LazareChkhartishvili@users.noreply.github.com>
2025-07-10 15:00:42 +0400

    Update api.ts

--- app/config/api.ts
+++ app/config/api.ts
@@ -14,8 +14,9 @@ interface RegistrationData {
 // API Configuration
 export const API_CONFIG = {
   // შეცვალე შენი backend URL-ით
-  BASE_URL: process.env.NEXT_PUBLIC_API_URL || "https://grs-bkbc.onrender.com",
-  // BASE_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000",
+  // BASE_URL: process.env.NEXT_PUBLIC_API_URL || "https://grs-bkbc.onrender.com",
+  BASE_URL: process.env.NEXT_PUBLIC_API_URL!,
+
   ENDPOINTS: {
     CATEGORIES: "/api/categories/with-subcategories",
     MAIN_CATEGORIES: "/api/categories",
